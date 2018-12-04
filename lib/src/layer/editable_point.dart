76a43490acce21e9b60d5ff86daf5635eb3666a1
Kenneth Gulbrandsøy <kenneth@discoos.org>
2018-12-04 23:10:01 +0100

    Added selection and delete on long press

--- lib/src/layer/editable_point.dart
+++ lib/src/layer/editable_point.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 import 'dart:collection';
-import 'dart:ui';
 import 'dart:core';
+import 'dart:ui';
 
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_map/src/map/map.dart';
 import 'package:latlong/latlong.dart' show LatLng;
 
@@ -33,6 +33,8 @@ class _EditablePointsWidgetState extends State<EditablePointsWidget> {
 
   final Map<int, LatLng> _dragging = HashMap();
 
+  int _selected;
+
   _EditablePointsWidgetState()
       : onPointChanged = StreamController.broadcast(),
         onMidPointChanged = StreamController.broadcast();
@@ -49,9 +51,16 @@ class _EditablePointsWidgetState extends State<EditablePointsWidget> {
     for (int i = 0; i < length; i++) {
       onMidPointChanged.add(i);
     }
+    _selected = index;
   }
 
   void _onPointTap(int index, LatLng point) {
+    setState(() {
+      _selected = index;
+    });
+  }
+
+  void _onPointLongPress(int index, LatLng point) {
     if (widget.points.length > 2) {
       setState(() {
         widget.points.removeAt(index);
@@ -62,6 +71,7 @@ class _EditablePointsWidgetState extends State<EditablePointsWidget> {
         for (int i = 0; i < length; i++) {
           onMidPointChanged.add(i);
         }
+        _selected = index - 1;
       });
     }
   }
@@ -69,6 +79,7 @@ class _EditablePointsWidgetState extends State<EditablePointsWidget> {
   void _onMidPointStart(int index, LatLng point) {
     _dragging[index] = point;
     widget.points.insert(index + 1, point);
+    _selected = index + 1;
   }
 
   void _onMidPointCancel(int index, LatLng point) {
@@ -151,10 +162,12 @@ class _EditablePointsWidgetState extends State<EditablePointsWidget> {
               radius: widget.radius,
               strokeWidth: widget.strokeWidth,
               isMidPoint: false,
+              isSelected: (_selected == i),
               index: i++,
               points: widget.points,
               onChanged: onPointChanged.stream,
               onTap: _onPointTap,
+              onLongPress: _onPointLongPress,
               onDragUpdate: _onPointUpdate,
             ))
         .toList();
@@ -168,6 +181,7 @@ class _EditablePointsWidgetState extends State<EditablePointsWidget> {
             radius: widget.radius,
             strokeWidth: widget.strokeWidth,
             isMidPoint: true,
+            isSelected: false,
             index: i++,
             points: _midPoints,
             onChanged: onMidPointChanged.stream,
@@ -181,40 +195,27 @@ class _EditablePointsWidgetState extends State<EditablePointsWidget> {
 
 typedef void PointTapCallback(int index, LatLng point);
 typedef void PointLongPressCallback(int index, LatLng point);
-typedef void PointDragStartCallback(int index, LatLng point);
-typedef void PointDragUpdateCallback(int index, LatLng point);
-typedef void PointDragCancelCallback(int index, LatLng point);
-typedef void PointDragEndCallback(int index, LatLng point);
+typedef void PointDragCallback(int index, LatLng point);
 typedef Offset TranslateFunction(Offset offset, bool toLocal);
 
 class EditableHandleWidget extends EditablePointWidget {
-  final MapState map;
-  final double radius;
-  final bool isMidPoint;
-  final double strokeWidth;
-  final int index;
-  final List<LatLng> points;
-  final Stream<int> onChanged;
-  final PointTapCallback onDragStart;
-  final PointDragUpdateCallback onDragUpdate;
-  final PointDragEndCallback onDragEnd;
-  final PointTapCallback onTap;
-  final PointLongPressCallback onLongPress;
-
   EditableHandleWidget({
-    this.map,
-    this.radius,
-    this.isMidPoint,
-    this.strokeWidth,
-    this.index,
-    this.points,
-    this.onChanged,
-    this.onDragStart,
-    this.onDragUpdate,
-    this.onDragEnd,
-    this.onTap,
-    this.onLongPress,
+    Key key,
+    @required map,
+    @required radius,
+    @required strokeWidth,
+    @required isMidPoint,
+    @required isSelected,
+    @required index,
+    @required points,
+    onChanged,
+    onDragStart,
+    onDragUpdate,
+    onDragEnd,
+    onTap,
+    onLongPress,
   }) : super(
+          key: key,
           map: map,
           size: Size.fromRadius(radius),
           builder: (BuildContext context) {
@@ -225,6 +226,7 @@ class EditableHandleWidget extends EditablePointWidget {
                       offset: Offset(radius, radius),
                       radius: radius,
                       isMidPoint: isMidPoint,
+                      isSelected: isSelected,
                       strokeWidth: strokeWidth),
                   size: Size.fromRadius(radius),
                 ),
@@ -249,61 +251,6 @@ class EditableHandleWidget extends EditablePointWidget {
         );
 }
 
-class HandlePainter extends CustomPainter {
-  final Offset offset;
-  final double radius;
-  final double strokeWidth;
-  final bool isMidPoint;
-
-  HandlePainter({this.offset, this.radius, this.strokeWidth, this.isMidPoint});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final rect = Offset.zero & size;
-    final double opacity = isMidPoint ? 0.4 : 0.8;
-    canvas.clipRect(rect);
-    final fill = Paint()
-      ..style = PaintingStyle.fill
-      ..color = Colors.white.withOpacity(opacity);
-
-    final line = Paint()
-      ..style = PaintingStyle.stroke
-      ..color = Colors.blue.withOpacity(opacity)
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = strokeWidth;
-
-    (isMidPoint ? _paintRect : _paintCircle)(canvas, radius, fill);
-    (isMidPoint ? _paintRect : _paintCircle)(
-        canvas, radius - strokeWidth / 2.0, line);
-
-    line.strokeWidth = strokeWidth / 2.0;
-
-    _paintCross(canvas, line);
-  }
-
-  void _paintCross(Canvas canvas, Paint paint) {
-    final Path path = Path();
-    Offset center = offset;
-    path.moveTo(center.dx, center.dy - 3.0);
-    path.lineTo(center.dx, center.dy + 3.0);
-    path.moveTo(center.dx - 3.0, center.dy);
-    path.lineTo(center.dx + 3.0, center.dy);
-    path.close();
-    canvas.drawPath(path, paint);
-  }
-
-  void _paintRect(Canvas canvas, double radius, Paint paint) {
-    canvas.drawRect(Rect.fromCircle(center: offset, radius: radius), paint);
-  }
-
-  void _paintCircle(Canvas canvas, double radius, Paint paint) {
-    canvas.drawCircle(offset, radius, paint);
-  }
-
-  @override
-  bool shouldRepaint(HandlePainter other) => true;
-}
-
 class EditablePointWidget extends StatefulWidget {
   final Size size;
   final MapState map;
@@ -312,14 +259,15 @@ class EditablePointWidget extends StatefulWidget {
   final List<LatLng> points;
   final TranslateFunction translate;
   final Stream<int> onChanged;
-  final PointDragUpdateCallback onDragStart;
-  final PointDragUpdateCallback onDragUpdate;
-  final PointDragUpdateCallback onDragCancel;
-  final PointDragUpdateCallback onDragEnd;
+  final PointDragCallback onDragStart;
+  final PointDragCallback onDragUpdate;
+  final PointDragCallback onDragCancel;
+  final PointDragCallback onDragEnd;
   final PointTapCallback onTap;
   final PointLongPressCallback onLongPress;
 
   EditablePointWidget({
+    Key key,
     @required this.map,
     @required this.size,
     @required this.builder,
@@ -333,7 +281,7 @@ class EditablePointWidget extends StatefulWidget {
     this.onDragEnd,
     this.onTap,
     this.onLongPress,
-  });
+  }) : super(key: key);
 
   @override
   EditablePointWidgetState createState() => EditablePointWidgetState();
@@ -473,4 +421,65 @@ class EditablePointWidgetState extends State<EditablePointWidget> {
       ),
     );
   }
+}
+
+class HandlePainter extends CustomPainter {
+  final Offset offset;
+  final double radius;
+  final double strokeWidth;
+  final bool isMidPoint;
+  final bool isSelected;
+
+  HandlePainter({
+    @required this.offset,
+    @required this.radius,
+    @required this.strokeWidth,
+    @required this.isMidPoint,
+    @required this.isSelected,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final rect = Offset.zero & size;
+    final double opacity = isMidPoint ? 0.4 : 0.8;
+    canvas.clipRect(rect);
+    final fill = Paint()
+      ..style = PaintingStyle.fill
+      ..color = Colors.white.withOpacity(opacity);
+
+    final line = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = Colors.blue.withOpacity(opacity)
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = isSelected ? strokeWidth * 3.0 : strokeWidth;
+
+    (isMidPoint ? _paintRect : _paintCircle)(canvas, offset, radius, fill);
+    (isMidPoint ? _paintRect : _paintCircle)(
+        canvas, offset, radius - line.strokeWidth / 2.0, line);
+
+    line.strokeWidth = strokeWidth / 2.0;
+
+    _paintCross(canvas, offset, line);
+  }
+
+  void _paintCross(Canvas canvas, Offset center, Paint paint) {
+    final Path path = Path();
+    path.moveTo(center.dx, center.dy - 3.0);
+    path.lineTo(center.dx, center.dy + 3.0);
+    path.moveTo(center.dx - 3.0, center.dy);
+    path.lineTo(center.dx + 3.0, center.dy);
+    path.close();
+    canvas.drawPath(path, paint);
+  }
+
+  void _paintRect(Canvas canvas, Offset center, double radius, Paint paint) {
+    canvas.drawRect(Rect.fromCircle(center: center, radius: radius), paint);
+  }
+
+  void _paintCircle(Canvas canvas, Offset center, double radius, Paint paint) {
+    canvas.drawCircle(center, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(HandlePainter other) => true;
 }
